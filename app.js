c4d27a3f87771c423fd242c41ca54ef7fc88eb30
Mujaahid Abdul H <mujaahid1996@gmail.com>
2018-03-30 02:27:01 +0700

    add existing

--- app.js
+++ app.js
@@ -3,62 +3,76 @@ var express = require('express'),
     request = require('request'),
     app = express(),
     bodyParser = require('body-parser'),
+    // port = process.env.PORT ,
     port = process.env.PORT || 8829,
     env = process.env.NODE_ENV || 'development',
     key = "805b2290e31d0da7cc708cd6cc3b1ec5",
     token = "acffb162bfe3d95886eb82a119fcf4a23353c6c01620d3a0280d221e0a29ff95";
 
+var Trello = require("trello");
+var trello = new Trello(key, token);
+
+// check via browser
+app.get('/', (req, res) => res.send('app.js is running'));
 
 // Allows us to easily read the payload from the webhook
 app.use(bodyParser.json());
 
-// Only act when a specific route is called
-// This reduces malicious / accidental use
-app.all("", function (req, res, next) {
+// detect action and trigger  
+app.all("/", function (req, res, next) {
 
     var trelloAction = req.body.action.type;
 
-console.log("" + JSON.stringify(req.body));
-console.log("-------------------- \n ");  
+    console.log("" + JSON.stringify(req.body));
+    console.log("-------------------- \n ");  
 
-    // new card
-    if (trelloAction == "createCard") {            
+    /*
+    di board Utama list saya, klo ada card baru
+    add card baru juga di list Utama : 5aa17052cbdf4f3d7149efc5
+    */
+
+    // card baru
+    if (trelloAction == "createCard" ) {            
         console.log("" + JSON.stringify(req.body));
         
         console.log("-------------------- \n ");               
         console.log("ada card baru");
         console.log("-------------------- ");
         var titleCard = JSON.stringify(req.body.action.data.card.name);
+        var titleBoard = JSON.stringify(req.body.action.data.board.name);
         console.log(titleCard);
-    }
-
-    // update deskripsi
-    if (trelloAction == "updateCard" && 
-        req.body.action.data.old.desc == "" 
-        ) {            
-        console.log("" + JSON.stringify(req.body));
-        
-        console.log("-------------------- \n ");               
-        console.log("ada deskripsi baru");
+        console.log(titleBoard);
         console.log("-------------------- ");
-        var titleCard = JSON.stringify(req.body.action.data.card.desc);
-        console.log(titleCard);
+
+        // add new card w/ npm trello
+        var newtitle = req.body.action.data.card.name;
+        var description = '';
+        var myListId = "5aa17052cbdf4f3d7149efc5"; // list Utama
+        trello.addCard(newtitle, description, myListId, function (error, trelloCard) {
+            if (error) {
+                console.log('Could not add card:', error);
+            } else {
+                console.log('Added card: \n', trelloCard);
+            }
+        });
+
     }
 
-    if( trelloAction != null || trelloAction != "" ){
-        console.log("-------------------- \n ");    
-        console.log( trelloAction);
-        console.log("--------------------");
+    // trace trello action
+    if(trelloAction != "" || trelloAction != null ){
+        console.log(""+JSON.stringify(trelloAction) );
+        console.log( JSON.stringify(req.body.action.data) );
     }
-
 
     res.send('OK');
+
 });
 
 
 // Standard NodeJS Listener
 var server = app.listen(port, function () {
-    var host = "trehook.localtunnel.me"; // custom
+    var host = "trehook.herokuapp.com"; // custom
+    // var host = "trehook.localtunnel.me"; // custom
     var port = server.address().port;
 
     console.log('Priority Enforcer listening at https://%s:%s in %s', host, port, env);
--- setupTes.js
+++ setupTes.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var mPort = process.env.PORT || 8829;
+// process.env.PORT
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! '
+	+'listening on port '
+	+ mPort))
 
-app.listen(8829, () => console.log('listening on port 8829!'))
+app.listen( mPort, () => console.log(
+	'listening on port '
+	+ mPort
+	+ ' !'))
+// app.listen(8829, () => console.log('listening on port 8829!'))
